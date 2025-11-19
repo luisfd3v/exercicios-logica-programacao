751efbd013250ac89bd748bb0a2d99d472a71412
luisfd3v <luisfd3v@gmail.com>
2025-11-19 06:43:34 -0300

    feat: exercicio 04

---
+++ exercicio04.py
@@ -0,0 +1,6 @@
+"""
+    Faça um algoritmo que receba um número inteiro e imprima na tela o seu antecessor e o seu sucessor.
+"""
+
+numero = int(input("Informe um número: "))
+print(f"O antecessor de {numero} é {numero-1}, e o sucessor é {numero+1}")
