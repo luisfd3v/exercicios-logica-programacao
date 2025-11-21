e0410fdc88e271d72c0a0c5bae498e417434bc76
luisfd3v <luisfd3v@gmail.com>
2025-11-21 06:50:37 -0300

    feat: exercicio 21

---
+++ exercicio21.py
@@ -0,0 +1,9 @@
+"""
+    Faça um algoritmo que mostre um valor aleatório entre 0 e 100.
+"""
+
+from random import randint
+
+numero_aleatorio = randint(0, 100)
+
+print(f"Número aleatório entre 0 e 100: {numero_aleatorio}")
